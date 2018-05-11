dc9aa0365e6779d628723a648de7f393f6502c80
monohal <monomonocasino@gmail.com>
2018-05-12 02:52:04 +0900

    Practice contest A
    100	AC	95 ms	21204 KB

---
+++ src/main/java/yuta4030/com/github/practice_contest/A.java
@@ -0,0 +1,18 @@
+package yuta4030.com.github.practice_contest;
+
+import java.util.Scanner;
+
+public class A {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        // 整数の入力
+        int a = sc.nextInt();
+        // スペース区切りの整数の入力
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        // 文字列の入力
+        String s = sc.next();
+        // 出力
+        System.out.println((a+b+c) + " " + s);
+    }
+}
