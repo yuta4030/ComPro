51a9370b9abc48d6b1c537c20d3459e602d68747
monohal <monomonocasino@gmail.com>
2018-05-13 04:23:40 +0900

    B - Exponential
    AtCoder Beginner Contest 097
    200	586 Byte		112 ms	21844 KB

--- src/main/java/yuta4030/com/github/AtCoderBeginnerContest/_097/Exponential.java
+++ src/main/java/yuta4030/com/github/AtCoderBeginnerContest/_097/Exponential.java
@@ -1,4 +1,25 @@
 package yuta4030.com.github.AtCoderBeginnerContest._097;
+/*
+https://beta.atcoder.jp/contests/abc097/tasks/abc097_b
+ */
+
+
+import java.util.Scanner;
 
 public class Exponential {
+    public static void main(String[] args) {
+        //99999は門番
+        int[] check = {1, 4, 8, 9, 16, 25, 27, 32, 36, 49, 64, 81, 100, 121, 125, 128, 144, 169, 196, 216, 225, 243, 256, 289, 324, 343, 361, 400, 441, 484, 512, 529, 576, 625, 676, 729, 784, 841, 900, 961, 1000, 99999};
+
+        Scanner sc = new Scanner(System.in);
+        int max = sc.nextInt();
+
+        for (int i = 0; i < check.length; i++) {
+            if (max < check[i]) {
+                System.out.println(check[i - 1]);
+                return;
+            }
+        }
+    }
+
 }
