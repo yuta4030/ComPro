f788c2f8099b261f4d50f0cd4fffddf467f83084
monohal <monomonocasino@gmail.com>
2018-05-24 23:31:35 +0900

    c++環境

---
+++ java/src/main/cpp/yuta4030/com/github/AtCoderBeginnerContest/_094/CatsAndDogs.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long unsigned int ll;
+
+#define EPS (1e-7)
+#define INF (1e9)
+#define PI (acos(-1))
+
+int main() {
+    cin >> a;
+    cin >> b;
+    cin >> c;
+    
+    cout << a + b + c << endl;
+    return ;
+}
